f727df43047ec17238462ff6d0439007944d6dc4
ja11sop <ja11sop@yahoo.co.uk>
2016-03-17 18:37:25 +0000

    Add c++1z to allowable standard versions

--- cuppa/methods/stdcpp.py
+++ cuppa/methods/stdcpp.py
@@ -14,13 +14,15 @@ from cuppa.log import logger
 
 class StdCppMethod:
 
-    stdcpp_choices = ( "c++98", "c++03", "c++0x", "c++11", "c++1y", "c++14" )
+    stdcpp_choices = ( "c++98", "c++03", "c++0x", "c++11", "c++1y", "c++14", "c++1z" )
 
     @classmethod
     def add_options( cls, add_option ):
 
         add_option( '--stdcpp', dest='stdcpp', choices=cls.stdcpp_choices, nargs=1, action='store',
-            help="Use this option to override the default language compliance of your cpp compiler which by dafault is the highest compliance available. Value may be one of {}".format( str(cls.stdcpp_choices) ) )
+            help="Use this option to override the default language compliance of your cpp compiler"
+                 " which by dafault is the highest compliance available. STDCPP may be one of {}"
+                 .format( str(cls.stdcpp_choices) ) )
 
     @classmethod
     def get_options( cls, env ):
