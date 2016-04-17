6be2e5f61e88f195747fba07632e6338c1bd5852
ja11sop <ja11sop@yahoo.co.uk>
2016-04-17 23:36:08 +0100

    Add CopyFiles() and CopyFilesAs() methods that notify progress
    This allows generators (for example) to be aware of the files

---
+++ cuppa/methods/copyfiles.py
@@ -0,0 +1,26 @@
+
+#          Copyright Jamie Allsop 2016
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+
+#-------------------------------------------------------------------------------
+#   CopyFilesMethod
+#-------------------------------------------------------------------------------
+
+import cuppa.progress
+
+from SCons.Script import Flatten
+
+
+class CopyFilesMethod:
+
+    def __call__( self, env, target, source ):
+        installed_files = env.Install( target, source )
+        cuppa.progress.NotifyProgress.add( env, installed_files )
+        return installed_files
+
+    @classmethod
+    def add_to_env( cls, cuppa_env ):
+        cuppa_env.add_method( "CopyFiles", cls() )
+
---
+++ cuppa/methods/copyfilesas.py
@@ -0,0 +1,26 @@
+
+#          Copyright Jamie Allsop 2016
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+
+#-------------------------------------------------------------------------------
+#   CopyFilesAsMethod
+#-------------------------------------------------------------------------------
+
+import cuppa.progress
+
+from SCons.Script import Flatten
+
+
+class CopyFilesAsMethod:
+
+    def __call__( self, env, target, source ):
+        installed_files = env.InstallAs( target, source )
+        cuppa.progress.NotifyProgress.add( env, installed_files )
+        return installed_files
+
+    @classmethod
+    def add_to_env( cls, cuppa_env ):
+        cuppa_env.add_method( "CopyFilesAs", cls() )
+
