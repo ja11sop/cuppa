8455747c531b69314955e55c858bdf59fae84890
ja11sop <ja11sop@yahoo.co.uk>
2016-05-07 19:52:09 +0100

    By default make relative folders sub-folders of "final_dir", not "working_dir"

--- cuppa/methods/copyfiles.py
+++ cuppa/methods/copyfiles.py
@@ -8,13 +8,17 @@
 #   CopyFilesMethod
 #-------------------------------------------------------------------------------
 
+import os.path
 import cuppa.progress
 
 
 class CopyFilesMethod:
 
     def __call__( self, env, target, source ):
-        installed_files = env.Install( target, source )
+        destination_folder = target
+        if not os.path.isabs( destination_folder ):
+            destination_folder = os.path.join( env['abs_final_dir'], destination_folder )
+        installed_files = env.Install( destination_folder, source )
         cuppa.progress.NotifyProgress.add( env, installed_files )
         return installed_files
 
