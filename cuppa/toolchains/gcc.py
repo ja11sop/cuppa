587a5924a7bf4de86f703456d6b96c3d933f9cdc
ja11sop <ja11sop@yahoo.co.uk>
2016-05-07 19:45:39 +0100

    Fix required from/by compiler output regex

--- cuppa/toolchains/gcc.py
+++ cuppa/toolchains/gcc.py
@@ -501,8 +501,8 @@ class Gcc(object):
             'column'    : None,
         },
         {
-            'title'     : "Required From",
-            'regex'     : r"([][{}() \t#%$~\w&_:+/\.-]+)(:([0-9]+):[0-9]+)(:[ \t]+required from .*)",
+            'title'     : "Required",
+            'regex'     : r"([][{}() \t#%$~\w&_:+/\.-]+)(:([0-9]+):[0-9]+)(:[ \t]+[Rr]equired (?:from|by) .*)",
             'meaning'   : 'message',
             'highlight' : set( [ 1, 2 ] ),
             'display'   : [ 1, 2, 4 ],
