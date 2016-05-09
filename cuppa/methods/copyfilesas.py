0abf7e7a4779bf06d1475d4cb6eb536a8760eaae
ja11sop <ja11sop@yahoo.co.uk>
2016-05-09 09:58:51 +0100

    By default relative paths are copied to the final dir of the project

--- cuppa/methods/copyfilesas.py
+++ cuppa/methods/copyfilesas.py
@@ -8,13 +8,22 @@
 #   CopyFilesAsMethod
 #-------------------------------------------------------------------------------
 
+from SCons.Script import Flatten
+
 import cuppa.progress
 
 
 class CopyFilesAsMethod:
 
     def __call__( self, env, target, source ):
-        installed_files = env.InstallAs( target, source )
+
+        destinations = []
+        for destination in Flatten([target]):
+            if not os.path.isabs( destination ):
+                destination = os.path.join( env['abs_final_dir'], destination )
+            destinations.append( destination )
+
+        installed_files = env.InstallAs( destinations, source )
         cuppa.progress.NotifyProgress.add( env, installed_files )
         return installed_files
 
