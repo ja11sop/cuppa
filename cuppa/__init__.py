cffa37927cd8ff894e124f3ac7ae08c21f86a13d
ja11sop <ja11sop@yahoo.co.uk>
2016-05-09 10:00:23 +0100

    Import location_dependency as the old name also for compatibility

--- cuppa/__init__.py
+++ cuppa/__init__.py
@@ -1,5 +1,5 @@
 
-#          Copyright Jamie Allsop 2011-2015
+#          Copyright Jamie Allsop 2011-2016
 # Distributed under the Boost Software License, Version 1.0.
 #    (See accompanying file LICENSE_1_0.txt or copy at
 #          http://www.boost.org/LICENSE_1_0.txt)
@@ -18,3 +18,4 @@ def add_option( *args, **kwargs ):
 import cuppa.build_with_location
 
 from cuppa.build_with_location import location_dependency
+from cuppa.build_with_location import location_dependency as header_library_dependency
