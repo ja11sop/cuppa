4ef0af6a3257e106a1e2792230b022eea3729124
ja11sop <ja11sop@yahoo.co.uk>
2017-07-17 22:12:44 +0100

    Initial support for #13 - only fail if the VCS is expected

--- cuppa/scms/git.py
+++ cuppa/scms/git.py
@@ -1,5 +1,5 @@
 
-#          Copyright Jamie Allsop 2014-2015
+#          Copyright Jamie Allsop 2014-2017
 # Distributed under the Boost Software License, Version 1.0.
 #    (See accompanying file LICENSE_1_0.txt or copy at
 #          http://www.boost.org/LICENSE_1_0.txt)
@@ -11,42 +11,106 @@
 import subprocess
 import shlex
 import os
+import re
 from exceptions import Exception
 
-
-class GitException(Exception):
-    def __init__(self, value):
-        self.parameter = value
-    def __str__(self):
-        return repr(self.parameter)
+from cuppa.log import logger
+from cuppa.colourise import as_notice, as_info, colour_items
 
 
-def info( path ):
-    if not path:
-        raise GitException("No working copy path specified for calling git commands with.")
+class Git:
 
-    url        = None
-    repository = None
-    branch     = None
-    revision   = None
+    class Error(Exception):
+        def __init__(self, value):
+            self.parameter = value
+        def __str__(self):
+            return repr(self.parameter)
 
-    if not os.path.exists( os.path.join( path, ".git" ) ):
-        raise GitException("Not a Git working copy")
 
-    try:
-        command = "git describe --always"
-        revision = subprocess.check_output( shlex.split( command ), stderr=subprocess.STDOUT, cwd=path ).strip()
+    @classmethod
+    def vc_type( cls ):
+        return "git"
 
-        command = "git symbolic-ref HEAD"
-        branch = subprocess.check_output( shlex.split( command ), stderr=subprocess.STDOUT, cwd=path )
-        branch = branch.replace( "refs/heads/", "" ).strip()
 
-        command = "git config --get remote.origin.url"
-        repository = subprocess.check_output( shlex.split( command ), stderr=subprocess.STDOUT, cwd=path ).strip()
-        url = repository
+    @classmethod
+    def binary( cls ):
+        return "git"
 
-    except subprocess.CalledProcessError:
-        raise GitException("Not a Git working copy")
 
-    return url, repository, branch, revision
+    @classmethod
+    def execute_command( cls, command, path ):
+        try:
+            return subprocess.check_output( shlex.split( command ), stderr=subprocess.STDOUT, cwd=path )
+        except subprocess.CalledProcessError:
+            raise cls.Error("Command [{command}] failed".format( command=str(command) ) )
+        except OSError:
+            raise cls.Error("Binary [{git}] is not available".format(
+                    git=cls.binary()
+            ) )
+
+
+    @classmethod
+    def get_branch( cls, path ):
+        branch = None
+        try:
+            result = cls.execute_command( "{git} symbolic-ref HEAD".format( git=cls.binary() ), path )
+            branch = result.replace( "refs/heads/", "" ).strip()
+            logger.trace( "Branch (using symbolic-ref) for [{}] is [{}]".format( as_notice(path), as_info(branch) ) )
+            return branch
+        except cls.Error:
+            pass
+
+        # In case we have a detached head we can fallback to this
+        result = cls.execute_command( "{git} show -s --pretty=\%d HEAD".format( git=cls.binary() ), path )
+        match = re.search( r'[(]HEAD[^,]*[,] (?P<branches>[^)]+)[)]', result )
+        if match:
+            branches = [ b.strip() for b in match.group("branches").split(',') ]
+            logger.trace( "Branches (using show) for [{}] are [{}]".format( as_notice(path), colour_items(branches) ) )
+            if len(branches) == 1:
+                branch = branches[0].split('/')[1]
+            else:
+                branch = branches[-2].split('/')[1]
+            logger.trace( "Branch (using show) for [{}] is [{}]".format( as_notice(path), as_info(branch) ) )
+        else:
+            logger.warn( "No branch found from [{}]".format( result ) )
+
+        return branch
+
+
+
+    @classmethod
+    def info( cls, path ):
+        if not path:
+            raise cls.Error("No working copy path specified for calling git commands with.")
+
+        url        = None
+        repository = None
+        branch     = None
+        revision   = None
+
+        if not os.path.exists( os.path.join( path, ".git" ) ):
+            raise cls.Error("Not a Git working copy")
+
+        command = None
+        try:
+            command = "{git} describe --always".format( git=cls.binary() )
+            revision = subprocess.check_output( shlex.split( command ), stderr=subprocess.STDOUT, cwd=path ).strip()
+
+            branch = cls.get_branch( path )
+
+            command = "{git} config --get remote.origin.url".format( git=cls.binary() )
+            repository = subprocess.check_output( shlex.split( command ), stderr=subprocess.STDOUT, cwd=path ).strip()
+            url = repository
+
+        except subprocess.CalledProcessError:
+            raise cls.Error("Git command [{command}] failed".format(
+                    command=str(command)
+            ) )
+
+        except OSError:
+            raise cls.Error("Git binary [{git}] is not available".format(
+                    git=cls.binary()
+            ) )
+
+        return url, repository, branch, revision
 
--- cuppa/scms/mercurial.py
+++ cuppa/scms/mercurial.py
@@ -1,5 +1,5 @@
 
-#          Copyright Jamie Allsop 2015-2015
+#          Copyright Jamie Allsop 2015-2017
 # Distributed under the Boost Software License, Version 1.0.
 #    (See accompanying file LICENSE_1_0.txt or copy at
 #          http://www.boost.org/LICENSE_1_0.txt)
@@ -14,47 +14,66 @@ import os
 from exceptions import Exception
 
 
-class MercurialException(Exception):
-    def __init__(self, value):
-        self.parameter = value
-    def __str__(self):
-        return repr(self.parameter)
+
+class Mercurial:
+
+    class Error(Exception):
+        def __init__(self, value):
+            self.parameter = value
+        def __str__(self):
+            return repr(self.parameter)
 
 
-def info( path ):
-    if not path:
-        raise MercurialException("No working copy path specified for calling hg commands with.")
+    @classmethod
+    def vc_type( cls ):
+        return "hg"
 
-    url        = None
-    repository = None
-    branch     = None
-    revision   = None
 
-    if not os.path.exists( os.path.join( path, ".hg" ) ):
-        raise MercurialException("Not a Mercurial working copy")
+    @classmethod
+    def binary( cls ):
+        return "hg"
 
-    try:
-        command = "hg summary"
-        summary = subprocess.check_output( shlex.split( command ), stderr=subprocess.STDOUT, cwd=path ).strip().split('\n')
 
-        revision = ""
-        branch   = ""
-        for line in summary:
-            if not revision and line.startswith( 'parent: ' ):
-                revision = line.replace( 'parent: ', '' )
-                if branch:
-                    break
-            elif not branch and line.startswith( 'branch: ' ):
-                branch = line.replace( 'branch: ', '' )
-                if revision:
-                    break
+    @classmethod
+    def info( cls, path ):
+        if not path:
+            raise cls.Error("No working copy path specified for calling hg commands with.")
 
-        command = "hg path"
-        repository = subprocess.check_output( shlex.split( command ), stderr=subprocess.STDOUT, cwd=path ).strip().split('=')[1]
-        url = repository
+        url        = None
+        repository = None
+        branch     = None
+        revision   = None
 
-    except subprocess.CalledProcessError:
-        raise MercurialException("Not a Mercurial working copy")
+        if not os.path.exists( os.path.join( path, ".hg" ) ):
+            raise cls.Error("Not a Mercurial working copy")
 
-    return url, repository, branch, revision
+        try:
+            command = "{hg} summary".format( hg=cls.binary() )
+            summary = subprocess.check_output( shlex.split( command ), stderr=subprocess.STDOUT, cwd=path ).strip().split('\n')
+
+            revision = ""
+            branch   = ""
+            for line in summary:
+                if not revision and line.startswith( 'parent: ' ):
+                    revision = line.replace( 'parent: ', '' )
+                    if branch:
+                        break
+                elif not branch and line.startswith( 'branch: ' ):
+                    branch = line.replace( 'branch: ', '' )
+                    if revision:
+                        break
+
+            command = "{hg} path".format( hg=cls.binary() )
+            repository = subprocess.check_output( shlex.split( command ), stderr=subprocess.STDOUT, cwd=path ).strip().split('=')[1]
+            url = repository
+
+        except subprocess.CalledProcessError:
+            raise cls.Error("Not a Mercurial working copy")
+
+        except OSError:
+            raise cls.Error("Mercurial binary [{hg}] is not available".format(
+                    hg=cls.binary()
+            ) )
+
+        return url, repository, branch, revision
 
