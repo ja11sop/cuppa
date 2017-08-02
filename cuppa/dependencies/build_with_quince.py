91d840c67e46af7de0c8739090196486bf19950b
ja11sop <ja11sop@yahoo.co.uk>
2017-08-02 22:25:08 +0100

    Update build_with_quince to use new library capabilities and simplify code

--- cuppa/dependencies/build_with_quince.py
+++ cuppa/dependencies/build_with_quince.py
@@ -13,11 +13,17 @@ import subprocess
 import shlex
 import platform
 import glob
+import inspect
+
+from exceptions import Exception
 
 # Cuppa Imports
 import cuppa.location
 import cuppa.output_processor
 from cuppa.log import logger
+
+# Quince Imports
+from cuppa.build_with_location import location_dependency, LibraryMethod
 
 
 
@@ -28,82 +34,23 @@ class QuinceException(Exception):
         return repr(self.parameter)
 
 
-class QuinceLibraryMethod(object):
 
-    def __init__( self, location, src_path ):
-        self._location = location
-        self._src_path = src_path
+class build_with_quince( location_dependency( 'quince', sys_include="include", source_path="src", linktype="static" ) ):
 
+    def __init__( self, env, location, includes=[], sys_includes=[], source_path=None, linktype=None ):
+        super(build_with_quince,self).__init__( env, location, includes, sys_includes, source_path, linktype )
 
-    def __call__( self, env, linktype ):
+        def update_env( env ):
+            env.BuildWith('boost')
 
-        build_dir = os.path.join( self._location, env['build_dir'] )
-        final_dir = os.path.normpath( os.path.join( build_dir, env['final_dir'] ) )
-
-        env.BuildWith( 'boost' )
-
-        objects = []
-        for source in env.RecursiveGlob( "*.cpp", start=self._src_path, exclude_dirs=[ env['build_dir'] ] ):
-            rel_path = os.path.relpath( str(source), self._location )
-            obj_path = os.path.join( build_dir, os.path.splitext( rel_path )[0] ) + env['OBJSUFFIX']
-            objects.append( env.Object( obj_path, source ) )
-
-        if linktype == "static":
-            return env.BuildStaticLib( "quince", objects, final_dir = final_dir )
-        else:
-            shared_lib = env.BuildSharedLib( "quince", objects, final_dir = final_dir )
-            return env.Install( env['abs_final_dir'], shared_lib )
-
-
-
-class build_with_quince(object):
-
-    _name = "quince"
-    _location = None
-
-    @classmethod
-    def add_options( cls, add_option ):
-        location_name = cls._name + "-location"
-        add_option( '--' + location_name, dest=location_name, type='string', nargs=1, action='store',
-                    help = cls._name + ' location to build against' )
-
-
-    @classmethod
-    def add_to_env( cls, env, add_dependency  ):
-        add_dependency( cls._name, cls.create )
-
-
-    @classmethod
-    def create( cls, env ):
-        if not cls._location:
-            location = env.get_option( cls._name + "-location" )
-            if not location:
-                logger.error( "Dependency not available - no location specified" )
-                return None
-            try:
-                cls._location = cuppa.location.Location( env, location )
-            except cuppa.location.LocationException as error:
-                logger.error(
-                    "Dependency not available - retrieving location failed with error [{}]."
-                    .format( str(error) )
-                )
-                return None
-        return build_with_quince( env )
-
-
-    def __init__( self, env ):
-        self._includes = [ os.path.join( self._location.local(), "include" ) ]
-        self._src_path = os.path.join( self._location.local(), "src" )
-
-        env.AddMethod( QuinceLibraryMethod( self._location.local(), self._src_path ), "QuinceLibrary" )
+        env.AddMethod( LibraryMethod( self, update_env ), "QuinceLibrary" )
 
 
     def __call__( self, env, toolchain, variant ):
-
-        env.AppendUnique( INCPATH = self._includes )
+        super(build_with_quince,self).__call__( env, toolchain, variant )
 
         env.AppendUnique( STATICLIBS = [
-                env.QuinceLibrary( 'static' ),
+                env.QuinceLibrary(),
                 env.BoostStaticLibs( [
                         'filesystem',
                         'system',
@@ -112,88 +59,43 @@ class build_with_quince(object):
         ] )
 
 
-    @classmethod
-    def name( cls ):
-        return cls._name
 
-    def version( self ):
-        return str(self._location.version())
+class quince_postgresql( location_dependency( 'quince-postgresql', sys_include="include", source_path="src", linktype="static" ) ):
 
-    def repository( self ):
-        return self._location.repository()
+    def __init__( self, env, location, includes=[], sys_includes=[], source_path=None, linktype=None ):
+        super(quince_postgresql,self).__init__( env, location, includes, sys_includes, source_path, linktype )
 
-    def branch( self ):
-        return self._location.branch()
+        self._flags = self.get_flags( location )
 
-    def revisions( self ):
-        return self._location.revisions()
+        def update_env( env ):
+            env.BuildWith('boost')
+
+        env.AddMethod( LibraryMethod( self, update_env ), "QuincePostgresqlLibrary" )
+        #env.AddMethod( QuinceLibraryMethod( self ), "QuincePostgresqlLibrary" )
 
 
+    def __call__( self, env, toolchain, variant ):
+        super(quince_postgresql,self).__call__( env, toolchain, variant )
 
-class QuincePostgresqlLibraryMethod(object):
+        env.AppendUnique( INCPATH     = self._flags['INCPATH'] )
+        env.AppendUnique( LIBPATH     = self._flags['LIBPATH'] )
+        env.AppendUnique( DYNAMICLIBS = self._flags['DYNAMICLIBS'] )
 
-    def __init__( self, location, src_path ):
-        self._location = location
-        self._src_path = src_path
+        quince_postgresql_lib = env.QuincePostgresqlLibrary()
+        quince_lib = env.QuinceLibrary()
 
+        env.Append( STATICLIBS  = [
+                quince_postgresql_lib,
+                quince_lib,
+                env.BoostStaticLibs( [ 'date_time' ] ),
+        ] )
 
-    def __call__( self, env, linktype ):
-        build_dir = os.path.join( self._location, env['build_dir'] )
-        final_dir = os.path.normpath( os.path.join( build_dir, env['final_dir'] ) )
-
-        env.BuildWith( 'boost' )
-
-        objects = []
-        for source in env.RecursiveGlob( "*.cpp", start=self._src_path, exclude_dirs=[ env['build_dir'] ] ):
-            rel_path = os.path.relpath( str(source), self._location )
-            obj_path = os.path.join( build_dir, os.path.splitext( rel_path )[0] ) +env['OBJSUFFIX']
-            objects.append( env.Object( obj_path, source ) )
-
-        if linktype == "static":
-            return env.BuildStaticLib( "quince-postgresql", objects, final_dir = final_dir )
-        else:
-            shared_lib = env.BuildSharedLib( "quince-postgresql", objects, final_dir = final_dir )
-            return env.Install( env['abs_final_dir'], shared_lib )
-
-
-
-class quince_postgresql(object):
-
-    _name = "quince-postgresql"
-    _location = None
 
     @classmethod
-    def add_options( cls, add_option ):
+    def get_flags( cls, location ):
 
-        location_name = cls._name + "-location"
-        add_option( '--' + location_name, dest=location_name, type='string', nargs=1, action='store',
-                    help = cls._name + ' location to build against' )
-
-    @classmethod
-    def add_to_env( cls, env, add_dependency  ):
-        add_dependency( cls._name, cls.create )
-
-    @classmethod
-    def create( cls, env ):
-        if not cls._location:
-            location = env.get_option( cls._name + "-location" )
-            if not location:
-                logger.error( "postgresql: Dependency not available - no location specified" )
-                return None
-            try:
-                cls._location = cuppa.location.Location( env, location )
-            except cuppa.location.LocationException as error:
-                logger.error(
-                    "postgresql: Dependency not available - retrieving location failed with error [{}]."
-                    .format( str(error) )
-                )
-                return None
-        return quince_postgresql( env )
-
-
-    def __init__( self, env ):
-        self._flags = {}
-        self._flags['INCPATH'] = [ os.path.join( self._location.local(), "include" ) ]
+        flags = {}
+        flags['INCPATH'] = [ os.path.join( location.local(), "include" ) ]
 
         pg_config = "pg_config"
         if platform.system() == "Windows":
@@ -212,117 +114,60 @@ class quince_postgresql(object):
         if cuppa.output_processor.command_available( pg_config ):
             command = "{pg_config} --includedir".format( pg_config = pg_config )
             libpq_include = subprocess.check_output( shlex.split( command ), stderr=subprocess.STDOUT ).strip()
-            self._flags['INCPATH'].append( libpq_include )
+            flags['INCPATH'].append( libpq_include )
 
             command = "{pg_config} --libdir".format( pg_config = pg_config )
             libpq_libpath = subprocess.check_output( shlex.split( command ), stderr=subprocess.STDOUT ).strip()
-            self._flags['LIBPATH'] = [ libpq_libpath ]
+            flags['LIBPATH'] = [ libpq_libpath ]
         else:
             logger.error( "postgresql: pg_config not available so cannot determine LIBPATH for postgres libraries" )
             raise QuinceException( "pg_config not available" )
 
-        self._flags['DYNAMICLIBS'] = [ 'pq' ]
+        flags['DYNAMICLIBS'] = [ 'pq' ]
 
-        self._src_path = os.path.join( self._location.local(), "src" )
+        return flags
 
-        env.AddMethod( QuincePostgresqlLibraryMethod( self._location.local(), self._src_path ), "QuincePostgresqlLibrary" )
+
+
+class quince_sqlite( location_dependency( 'quince-sqlite', sys_include="include", source_path="src", linktype="static" ) ):
+
+
+    def __init__( self, env, location, includes=[], sys_includes=[], source_path=None, linktype=None ):
+        super(quince_sqlite,self).__init__( env, location, includes, sys_includes, source_path, linktype )
+
+        self._flags = self.get_flags( location )
+
+        def update_env( env ):
+            env.BuildWith('boost')
+
+        env.AddMethod( LibraryMethod( self, update_env ), "QuinceSqliteLibrary" )
+        #env.AddMethod( QuinceLibraryMethod( self ), "QuinceSqliteLibrary" )
 
 
     def __call__( self, env, toolchain, variant ):
-        env.AppendUnique( INCPATH     = self._flags['INCPATH'] )
-        env.AppendUnique( LIBPATH     = self._flags['LIBPATH'] )
-        env.AppendUnique( DYNAMICLIBS = self._flags['DYNAMICLIBS'] )
+        super(quince_sqlite,self).__call__( env, toolchain, variant )
 
-        quince_postgresql_lib = env.QuincePostgresqlLibrary('static')
-        quince_lib = env.QuinceLibrary('static')
+        for name, flags in self._flags.iteritems():
+            if flags:
+                env.AppendUnique( **{ name: flags } )
+
+        quince_sqlite_lib = env.QuinceSqliteLibrary()
+        quince_lib = env.QuinceLibrary()
 
         env.Append( STATICLIBS  = [
-                quince_postgresql_lib,
+                quince_sqlite_lib,
                 quince_lib,
-                env.BoostStaticLibs( [ 'date_time' ] ),
+                env.BoostStaticLibs( [
+                    'date_time',
+                    'filesystem',
+                ] ),
         ] )
 
 
     @classmethod
-    def name( cls ):
-        return cls._name
+    def get_flags( cls, location ):
 
-    def version( self ):
-        return str(self._location.version())
-
-    def repository( self ):
-        return self._location.repository()
-
-    def branch( self ):
-        return self._location.branch()
-
-    def revisions( self ):
-        return self._location.revisions()
-
-
-class QuinceSqliteLibraryMethod(object):
-
-    def __init__( self, location, src_path ):
-        self._location = location
-        self._src_path = src_path
-
-
-    def __call__( self, env, linktype ):
-        build_dir = os.path.join( self._location, env['build_dir'] )
-        final_dir = os.path.normpath( os.path.join( build_dir, env['final_dir'] ) )
-
-        env.BuildWith( 'boost' )
-
-        objects = []
-        for source in env.RecursiveGlob( "*.cpp", start=self._src_path, exclude_dirs=[ env['build_dir'] ] ):
-            rel_path = os.path.relpath( str(source), self._location )
-            obj_path = os.path.join( build_dir, os.path.splitext( rel_path )[0] ) +env['OBJSUFFIX']
-            objects.append( env.Object( obj_path, source ) )
-
-        if linktype == "static":
-            return env.BuildStaticLib( "quince-sqlite", objects, final_dir = final_dir )
-        else:
-            shared_lib = env.BuildSharedLib( "quince-sqlite", objects, final_dir = final_dir )
-            return env.Install( env['abs_final_dir'], shared_lib )
-
-
-class quince_sqlite(object):
-
-    _name = "quince-sqlite"
-    _location = None
-
-    @classmethod
-    def add_options( cls, add_option ):
-
-        location_name = cls._name + "-location"
-        add_option( '--' + location_name, dest=location_name, type='string', nargs=1, action='store',
-                    help = cls._name + ' location to build against' )
-
-    @classmethod
-    def add_to_env( cls, env, add_dependency  ):
-        add_dependency( cls._name, cls.create )
-
-
-    @classmethod
-    def create( cls, env ):
-        if not cls._location:
-            location = env.get_option( cls._name + "-location" )
-            if not location:
-                logger.error( "sqlite: Dependency not available - no location specified" )
-                return None
-            try:
-                cls._location = cuppa.location.Location( env, location )
-            except cuppa.location.LocationException as error:
-                logger.error(
-                    "sqlite: Dependency not available - retrieving location failed with error [{}]."
-                    .format( str(error) )
-                )
-                return None
-        return quince_sqlite( env )
-
-
-    def __init__( self, env ):
-        self._flags = {}
+        flags = {}
         if cuppa.output_processor.command_available( "pkg-config"):
             command = "pkg-config --cflags --libs sqlite3"
             cflags = subprocess.check_output( shlex.split( command ), stderr=subprocess.STDOUT ).strip()
@@ -337,44 +182,7 @@ class quince_sqlite(object):
 
         if not 'INCPATH' in self._flags:
             self._flags['INCPATH'] = []
-        self._flags['INCPATH'].append( os.path.join( self._location.local(), "include" ) )
+        self._flags['INCPATH'].append( os.path.join( location.local(), "include" ) )
 
-        self._src_path = os.path.join( self._location.local(), "src" )
+        return flags
 
-        env.AddMethod( QuinceSqliteLibraryMethod( self._location.local(), self._src_path ), "QuinceSqliteLibrary" )
-
-
-    def __call__( self, env, toolchain, variant ):
-
-        for name, flags in self._flags.iteritems():
-            if flags:
-                env.AppendUnique( **{ name: flags } )
-
-        quince_sqlite_lib = env.QuinceSqliteLibrary('static')
-        quince_lib = env.QuinceLibrary('static')
-
-        env.Append( STATICLIBS  = [
-                quince_sqlite_lib,
-                quince_lib,
-                env.BoostStaticLibs( [
-                    'date_time',
-                    'filesystem',
-                ] ),
-        ] )
-
-
-    @classmethod
-    def name( cls ):
-        return cls._name
-
-    def version( self ):
-        return str(self._location.version())
-
-    def repository( self ):
-        return self._location.repository()
-
-    def branch( self ):
-        return self._location.branch()
-
-    def revisions( self ):
-        return self._location.revisions()
