6a71c948a2d31612a774c0d1809f39de07f5b872
ja11sop <ja11sop@yahoo.co.uk>
2019-02-12 17:34:57 +0000

    Add is_error method

--- cuppa/colourise.py
+++ cuppa/colourise.py
@@ -1,6 +1,6 @@
 
 #          Copyright Declan Traynor 2012
-#          Copyright Jamie Allsop 2012-2015
+#          Copyright Jamie Allsop 2012-2019
 # Distributed under the Boost Software License, Version 1.0.
 #    (See accompanying file LICENSE_1_0.txt or copy at
 #          http://www.boost.org/LICENSE_1_0.txt)
@@ -236,4 +236,7 @@ def emphasise_time_by_digit( time_text ):
 
 def colour_items( items, colour_func=as_notice ):
     return "'{}'".format( "', '".join( colour_func( item ) for item in items ) )
+
+def is_error( meaning ):
+    return meaning in ['error','failed','failure','aborted']
 
