805283893839d84b15b7c4015ed081463b6c2b87
Jonny Weir <jonny.weir@clearpool.io>
2020-06-05 13:04:35 +0100

    Always apply boost bug fix patches if available for specific boost version.

--- cuppa/dependencies/boost/patch_boost.py
+++ cuppa/dependencies/boost/patch_boost.py
@@ -18,47 +18,62 @@ from cuppa.colourise import as_info
 from cuppa.log       import logger
 
 
-
 def patched_boost_test( home ):
     patch_applied_path = os.path.join( home, "cuppa_test_patch_applied.txt" )
     return os.path.exists( patch_applied_path )
 
 
+def apply_patches_if_needed( patch_boost_test, home, version_string ):
+    if patch_boost_test:
+        apply_patch( "test_patch", home, version_string )
+    apply_patch( "bug_fix", home, version_string )
 
-def apply_patch_if_needed( home, version_string ):
 
-    patch_applied_path = os.path.join( home, "cuppa_test_patch_applied.txt" )
+def apply_patch( name, home, version_string ):
+    patch_applied_path = os.path.join( home, "cuppa_{}_applied.txt".format( name ) )
 
     expected_diff_file = os.path.join(
             os.path.split( __file__ )[0],
-            "boost_test_patch_{}.diff".format( version_string )
+            "boost_{}_{}.diff".format( name, version_string )
     )
 
     available_diff_files = sorted( glob.glob( os.path.join(
             os.path.split( __file__ )[0],
-            "boost_test_patch_*.diff"
+            "boost_{}_*.diff".format( name )
     ) ), reverse=True )
 
+    diff_file_to_use = None
     for diff_file in available_diff_files:
-        if diff_file <= expected_diff_file:
-            break
+        if name == "test_patch":
+            if diff_file <= expected_diff_file:
+                diff_file_to_use = diff_file
+                break
+        # XXX: Only apply bug fixes to the exact version of boost.
+        elif name == "bug_fix":
+            if diff_file == expected_diff_file:
+                diff_file_to_use = diff_file
+                break
 
-    logger.debug( "Using diff file [{}]".format( as_info( diff_file ) ) )
-
-    if os.path.exists( patch_applied_path ):
-        logger.debug( "[{}] already applied".format( as_info( diff_file ) ) )
+    if not diff_file_to_use:
+        logger.debug( "Nothing to patch for: {} {}".format( name, version_string ) )
         return
 
-    command = "patch --batch -p1 --input={}".format( diff_file )
+    logger.debug( "Using diff file [{}]".format( as_info( diff_file_to_use ) ) )
+
+    if os.path.exists( patch_applied_path ):
+        logger.debug( "[{}] already applied".format( as_info( diff_file_to_use ) ) )
+        return
+
+    command = "patch --batch -p1 --input={}".format( diff_file_to_use )
 
     logger.info( "Applying [{}] using [{}] in [{}]".format(
-            as_info( diff_file ),
+            as_info( diff_file_to_use ),
             as_info( command ),
             as_info( home )
     ) )
 
     if subprocess.call( shlex.split( command ), cwd=home ) != 0:
-        logger.error( "Could not apply [{}]".format( diff_file ) )
+        logger.error( "Could not apply [{}]".format( diff_file_to_use ) )
     else:
         with open( patch_applied_path, "w" ):
             logger.debug( 'Write "patch_applied_file" to record the patch has been applied.' )
