d157b7fc518ba22b278fa2bbe0e50044f69b0d28
Jonny Weir <jonny.weir@clearpool.io>
2021-08-18 10:48:30 +0100

    Bump current boost version.

--- cuppa/dependencies/boost/version_and_location.py
+++ cuppa/dependencies/boost/version_and_location.py
@@ -156,7 +156,7 @@ def get_boost_version( location ):
 
 
 def _determine_latest_boost_verion( offline ):
-    current_release = "1.76.0"
+    current_release = "1.77.0"
     if not offline:
         try:
             boost_version_url = 'https://www.boost.org/users/download/'
