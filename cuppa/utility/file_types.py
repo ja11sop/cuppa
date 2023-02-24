0df6437effe982183fd7e83636098821af3197d4
ja11sop <ja11sop@yahoo.co.uk>
2023-02-24 15:38:01 +0000

    Add missing file_types file

---
+++ cuppa/utility/file_types.py
@@ -0,0 +1,30 @@
+
+#          Copyright Jamie Allsop 2023-2023
+# Distributed under the Boost Software License, Version 1.0.
+#    (See accompanying file LICENSE_1_0.txt or copy at
+#          http://www.boost.org/LICENSE_1_0.txt)
+
+#-------------------------------------------------------------------------------
+#   File Types
+#-------------------------------------------------------------------------------
+
+
+def is_json_ext( ext ):
+    if ext:
+        return ext.lower() == ".json"
+    return False
+
+
+def is_yaml_ext( ext ):
+    if ext:
+        ext = ext.lower()
+        return ext == ".yaml" or ext == ".yml"
+    return False
+
+
+def is_json( file_path ):
+    return is_json_ext( os.path.splitext( file_path )[1] )
+
+
+def is_yaml( file_path ):
+    return is_yaml_ext(  os.path.splitext( file_path )[1] )
