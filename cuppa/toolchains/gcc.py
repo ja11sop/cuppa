e762df5ceecc20b13b038d09d720bd2071749945
ja11sop <ja11sop@yahoo.co.uk>
2025-10-11 17:50:57 +0100

    Add gcc15 support

--- cuppa/toolchains/gcc.py
+++ cuppa/toolchains/gcc.py
@@ -42,7 +42,8 @@ class Gcc(object):
     def supported_versions( cls ):
         return [
             "gcc",
-            "gcc14", "gcc141", "gcc142",
+            "gcc15", "gcc151", "gcc152",
+            "gcc14", "gcc141", "gcc142", "gcc143",
             "gcc13", "gcc131", "gcc132",
             "gcc12", "gcc121", "gcc122",
             "gcc11", "gcc111", "gcc112", "gcc113",
