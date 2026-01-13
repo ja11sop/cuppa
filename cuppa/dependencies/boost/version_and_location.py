7437aabc46aac264d8772c133e0136b56d98edef
Jonny Weir <jonny.weir@clearpool.io>
2026-01-13 11:17:10 +0000

    Set the current boost version to 1.90.0.

--- cuppa/dependencies/boost/version_and_location.py
+++ cuppa/dependencies/boost/version_and_location.py
@@ -32,7 +32,7 @@ from cuppa.dependencies.boost.patch_boost     import apply_patches_if_needed
 
 
 def current_boost_release():
-    return "1.89.0"
+    return "1.90.0"
 
 
 def boost_location_id( env ):
